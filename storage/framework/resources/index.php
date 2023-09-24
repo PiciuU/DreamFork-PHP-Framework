954ca10ba44e64e76dd3cd42a8dcbe5b2e22de0d
Patryk Strzałka <piciuubest@gmail.com>
2023-09-24 23:34:48 +0200

    Added basic CLI and support for maintenance mode

---
+++ storage/framework/resources/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('styles.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+        <h1>Application is currently in maintenance mode.</h1>
+        <h2>Please try again later.</h2>
+    </main>
+</body>
+
+</html>
